22f6681615bbe9d183a6b7eb634c05af82270cf1
Tushar Sharma <tushar8104@gmail.com>
2022-10-12 22:13:13 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+const nextButton = document.querySelector(".carousel-control-next-icon");
+const prevButton = document.querySelector(".carousel-control-prev-icon");
+
+const carouselOne = document.querySelector(".carousel-item-one");
+const carouselSec = document.querySelector(".carousel-item-sec");
+const carouselThree = document.querySelector(".carousel-item-three");
+
+const video = document.querySelector("video");
+
+const initialWrapper = document.querySelector(".initial-wrapper");
+
+const scrollDown = document.querySelector(".scroll-down-icon");
+
+let typed = new Typed(".auto_text", {
+  strings: [
+    "I am a Web Developer.",
+    "I write blogs.",
+    "I love to develop Web App.",
+    "I am a Web Developer.",
+    "I write blogs.",
+    "I love to develop Web App.",
+    "I am a Web Developer.",
+    "I write blogs.",
+    "I love to develop Web App.",
+    "I am a Web Developer.",
+    "I write blogs.",
+    "I love to develop Web App.",
+    "I am a Web Developer.",
+    "I write blogs.",
+    "I love to develop Web App.",
+    "I am a Web Developer.",
+    "I write blogs.",
+    "I love to develop Web App.",
+    "I am a Web Developer.",
+    "I write blogs.",
+    "I love to develop Web App.",
+    "I am a Web Developer.",
+    "I write blogs.",
+    "I love to develop Web App.",
+    "I am a Web Developer.",
+    "I write blogs.",
+    "I love to develop Web App.",
+    "I am a Web Developer.",
+    "I write blogs.",
+    "I love to develop Web App.",
+  ],
+  typeSpeed: 80,
+  backSpeed: 40,
+  loop: true,
+});
